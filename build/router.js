1a83e70a98158732a3b5305e54070354a3462842
root <coolpi07@gmail.com>
2020-04-10 15:17:55 +0800

    add bodyparser and custom.d.ts

--- build/router.js
+++ build/router.js
@@ -7,14 +7,23 @@ var express_1 = require("express");
 var crowller_1 = __importDefault(require("./crowller"));
 var dellAnalyzer_1 = __importDefault(require("./dellAnalyzer"));
 var router = express_1.Router();
+// 增加登录密码功能
 router.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send("\n    <html>\n      <body>\n        <form method=\"post\" action=\"/getData\">\n          <input type=\"password\" name=\"password\"/>\n          <button>\u63D0\u4EA4</button>\n        </form>\n      </body>\n    </html>\n  ");
+    // res.send('Hello World!');
 });
-router.get('/getData', function (req, res) {
-    var secret = 'x3b174jsx';
-    var url = "http://www.dell-lee.com/typescript/demo.html?secret=" + secret;
-    var analyzer = dellAnalyzer_1.default.getInstance();
-    new crowller_1.default(url, analyzer);
-    res.send('Get Data Success!');
+// 增加验证功能
+router.post('/getData', function (req, res) {
+    var password = req.body.password;
+    if (password === '123') {
+        var secret = 'x3b174jsx';
+        var url = "http://www.dell-lee.com/typescript/demo.html?secret=" + secret;
+        var analyzer = dellAnalyzer_1.default.getInstance();
+        new crowller_1.default(url, analyzer);
+        res.send('Get Data Success!');
+    }
+    else {
+        res.send(req.teacher + " password error");
+    }
 });
 exports.default = router;
--- src/router.ts
+++ src/router.ts
@@ -4,16 +4,38 @@ import DellAnalyzer from './dellAnalyzer';
 
 const router = Router();
 
+interface RequestWithBody extends Request {
+  body: {
+    [key: string]: string | undefined;
+  };
+}
+// 增加登录密码功能
 router.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send(`
+    <html>
+      <body>
+        <form method="post" action="/getData">
+          <input type="password" name="password"/>
+          <button>提交</button>
+        </form>
+      </body>
+    </html>
+  `);
+  // res.send('Hello World!');
 });
 
-router.get('/getData', (req: Request, res: Response) => {
-  const secret = 'x3b174jsx';
-  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-  const analyzer = DellAnalyzer.getInstance();
-  new Crowller(url, analyzer);
-  res.send('Get Data Success!');
+// 增加验证功能
+router.post('/getData', (req: RequestWithBody, res: Response) => {
+  const { password } = req.body;
+  if (password === '123') {
+    const secret = 'x3b174jsx';
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+    const analyzer = DellAnalyzer.getInstance();
+    new Crowller(url, analyzer);
+    res.send('Get Data Success!');
+  } else {
+    res.send(`${req.teacher} password error`);
+  }
 });
 
 export default router;
