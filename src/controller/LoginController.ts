854958e7472827a8d9450a5afdcb9f4c9177e58f
root <coolpi07@gmail.com>
2020-04-20 11:23:19 +0800

    生成控制器类组件

---
+++ src/controller/LoginController.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import { Request, Response } from 'express';
+import { get, controller } from './decorator';
+
+@controller
+class LoginController {
+  @get('/login')
+  login() {}
+  @get('/')
+  home(req: Request, res: Response) {
+    const isLogin = req.session ? req.session.isLogin : undefined;
+
+    if (isLogin) {
+      res.send(`
+      <html>
+        <body>
+          <a href="/showData">展示数据</a>
+          <a href="/getData">爬取数据</a>
+          <a href="/loginout">登出</a>
+        </body>
+      </html>
+    `);
+    } else {
+      res.send(`
+      <html>
+        <body>
+          <form method="post" action="/login">
+            <input type="password" name="password"/>
+            <button>登入</button>
+          </form>
+        </body>
+      </html>
+    `);
+    }
+  }
+}
